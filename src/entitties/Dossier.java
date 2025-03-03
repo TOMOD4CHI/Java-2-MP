a02db15067100cc56d91bbf14bb73bc65c32d806
XFrozedyX <86535219+YYYayoubXXX@users.noreply.github.com>
2025-03-03 21:39:57 +0100

    BF4

--- src/entitties/Dossier.java
+++ src/entitties/Dossier.java
@@ -1,18 +1,14 @@
 package entitties;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Dossier {
     private Long id;
     private Map<TypeDocument, Document> documents;
-    private List<Paiement> paiements;
 
     public Dossier() {
         this.documents = new HashMap<>();
-        this.paiements = new ArrayList<>();
     }
 
     public Map<TypeDocument, Document> getDocuments() {
@@ -29,13 +25,5 @@ public class Dossier {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public List<Paiement> getPaiements() {
-        return paiements;
-    }
-
-    public void setPaiements(List<Paiement> paiements) {
-        this.paiements = paiements;
     }
 }
