1a3e09c588b89a087c6183b711bb15f73b1f020e
TOMOD4CHI <saifpes21@gmail.com>
2025-03-03 21:41:23 +0100

    upd

--- src/entitties/Dossier.java
+++ src/entitties/Dossier.java
@@ -8,11 +8,9 @@ import java.util.Map;
 public class Dossier {
     private Long id;
     private Map<TypeDocument, Document> documents;
-    private List<Paiement> paiements;
 
     public Dossier() {
         this.documents = new HashMap<>();
-        this.paiements = new ArrayList<>();
     }
 
     public Map<TypeDocument, Document> getDocuments() {
@@ -29,13 +27,5 @@ public class Dossier {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public List<Paiement> getPaiements() {
-        return paiements;
-    }
-
-    public void setPaiements(List<Paiement> paiements) {
-        this.paiements = paiements;
     }
 }
